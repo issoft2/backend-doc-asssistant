5798b6459b401a188017b57149975f685ad9ce05
isaac <isaac@heliumhealth.com>
2025-12-19 23:05:31 +0100

    some adjustment for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // or: host: true
+    port: 5173,      // or any port you map from Docker
+  },
 })
